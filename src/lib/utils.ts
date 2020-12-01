2b78ae9335ec0b575a90a68834aab0cda918b254
samchencode <samchencode@gmail.com>
2020-12-01 11:08:53 -0500

    allow dom libraries

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,14 +1,22 @@
-export function deepClone<T, K extends keyof T> (value: T): T {
-  if(typeof value !== 'object') return value;
+export function deepClone<T, K extends keyof T>(value: T): T {
+  if (typeof value !== "object") return value;
 
   const keys = Object.getOwnPropertyNames(value) as K[];
 
   let result: { [key in K]: T[K] } = Array.isArray(value)
-  ? Array()
-  : Object.create({});
+    ? Array()
+    : Object.create({});
 
-  for( const key of keys ) {
-     result[key] = deepClone(value[key])
+  for (const key of keys) {
+    result[key] = deepClone(value[key]);
   }
   return result as T;
 }
+
+export const isNode = new Function(
+  "try {return this===global;}catch(e){return false;}"
+);
+
+export const isBrowser = new Function(
+  "try {return this===window;}catch(e){return false;}"
+);
