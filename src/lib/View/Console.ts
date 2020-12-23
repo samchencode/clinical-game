4f221b96870366a24681e72098e8efeb17d2fde3
samchencode <samchencode@gmail.com>
2020-12-23 09:12:22 -0500

    close app when view agent is done

--- src/lib/View/Console.ts
+++ src/lib/View/Console.ts
@@ -5,7 +5,7 @@ function ConsoleAgent(): IViewAgent {
   // FIXME: This doesnt clear options list before displaying new ones
 
   let lines: string[] = [];
-  let options: { name: string, cb: Function }[] = [];
+  let options: { name: string; cb: Function }[] = [];
 
   const rl = readline.createInterface({
     input: process.stdin,
@@ -15,14 +15,13 @@ function ConsoleAgent(): IViewAgent {
   rl.prompt();
 
   rl.on("line", (input: string) => {
-    if(Number.parseInt(input, 10) !== NaN) {
+    if (Number.parseInt(input, 10) !== NaN) {
       const optNum = Number.parseInt(input, 10);
-      const option = options[optNum - 1]
-      if(option) option.cb();
+      const option = options[optNum - 1];
+      if (option) option.cb();
     }
     rl.prompt();
   });
-
 
   function getRendererVisitor() {
     lines = [];
@@ -32,19 +31,24 @@ function ConsoleAgent(): IViewAgent {
       displayText: (s: string) => lines.push(s),
       displayImage: (url: string) => lines.push("\tSee image at: " + url),
       displayOption: (name: string, cb: Function) => {
-        options.push({ name, cb })
-      }
-    }
+        options.push({ name, cb });
+      },
+    };
   }
 
   return {
     renderer: getRendererVisitor,
     done: () => {
-      lines.forEach(l => console.log(l));
-      const optDisplay = options.map((v, k) => `${k+1}) ${v.name}`).join('\n')
+      lines.forEach((l) => console.log(l));
+      const optDisplay = options
+        .map((v, k) => `${k + 1}) ${v.name}`)
+        .join("\n");
       console.log(optDisplay);
     },
-    close: () => rl.close()
+    close: () => {
+      rl.close();
+      process.exit(0);
+    },
   };
 }
 
